31f659145ab1eedcb4e33dbc0dd5d397da18abaf
guilherme.ribeiro <guilherme.ribeiro@supero.com.br>
2015-02-22 23:47:40 -0300

    Annotation interceptor

--- ejb/src/main/java/br/org/base/interceptor/MemberAudit.java
+++ ejb/src/main/java/br/org/base/interceptor/MemberAudit.java
@@ -1,8 +1,13 @@
 package br.org.base.interceptor;
 
+import br.org.base.model.Member;
+
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
 
 @Interceptor
 @Audit
@@ -11,7 +16,32 @@ public class MemberAudit {
     public Object intercept(InvocationContext context) throws Exception {
 
         System.out.println("SimpleInterceptor - Logging BEFORE calling method :"+context.getMethod().getName() );
+        
+        String out = "";
+        String format = "";
+        Member param = null;
 
+        Audit audit2 = context.getMethod().getAnnotation(Audit.class);
+        String message = audit2.message();
+        format = message;
+        System.out.println(message);
+        
+        Collection col = Arrays.asList( context.getParameters());
+        Iterator<Member> memberIterator = col.iterator();
+        while(memberIterator.hasNext()){
+            Member member = memberIterator.next();
+            param = member;
+            System.out.println(member.getName());
+        }
+        
+        if(format != ""){
+            System.out.println(String.format("Member cnanged %s ", param.toString()));
+        }else {
+            System.out.println(String.format("%s cnanged %s ", param.getClass().getName(), param.toString()));
+        }
+
+
+        
         Object result = context.proceed();
 
         System.out.println("SimpleInterceptor - Logging AFTER calling method :"+context.getMethod().getName() );
